be7ff309ab11d60d1cd19d2e996fa1b9789169ce
rugi <rugi.eolos@gmail.com>
2021-06-16 11:45:25 -0500

    Se agrega WS y cliente para validar llaves

--- src/com/sps/Tester.java
+++ src/com/sps/Tester.java
@@ -1,13 +1,14 @@
 package com.sps;
 
-
-import java.io.InputStream;
-
+import com.sps.ws.client.SampleServiceService;
 import java.net.URL;
 
 import java.security.KeyStore;
 
 import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import java.util.Enumeration;
 
@@ -17,8 +18,8 @@ import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
 
-
 public class Tester {
+
     public Tester() {
         super();
     }
@@ -36,6 +37,13 @@ public class Tester {
             String aliasKey = (String) en.nextElement();
             Certificate c = ks.getCertificate(aliasKey);
             System.out.println("---> alias : " + aliasKey);
+            Date certExpiryDate = ((X509Certificate) c).getNotAfter();
+            SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");            
+            Date today = new Date();
+            long dateDiff = certExpiryDate.getTime() - today.getTime();
+            long expiresIn = dateDiff / (24 * 60 * 60 * 1000);
+            System.out.println("\t\t\t Fecha de expiracion:" + ft.format(certExpiryDate) +  "\tExpira en: " + expiresIn +" Dias");
+
             if (ks.isKeyEntry(aliasKey)) {
                 Certificate[] chain = ks.getCertificateChain(aliasKey);
                 System.out.println("---> tamano de la cadena: " + chain.length);
@@ -44,12 +52,12 @@ public class Tester {
                 }
             }
         }
-        
+
         // Le decimos a la clase que use el keystore cargado
         TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
         tmf.init(ks);
 
-        // Establecemos la conexión TLS hacia el servicio
+        // Establecemos la conexiï¿½n TLS hacia el servicio
         SSLContext context = SSLContext.getInstance("TLS");
         TrustManager[] trustManagers = tmf.getTrustManagers();
         context.init(null, trustManagers, null);
@@ -62,17 +70,19 @@ public class Tester {
         httpsCon.setSSLSocketFactory(sf);
         httpsCon.setRequestMethod("GET");
 
-       /* InputStream inStrm = httpsCon.getInputStream();
+        /* InputStream inStrm = httpsCon.getInputStream();
             System.out.println("\nContenido " + url);
             int ch;
               while (((ch = inStrm.read()) != -1)){
                 System.out.print((char) ch);
               inStrm.close();
             }*/
-
         System.out.println("Mensaje de respuesta: " + httpsCon.getResponseMessage() + ". HTTP Response Code: " + httpsCon.getResponseCode());
         System.out.println("Content-Type: " + httpsCon.getHeaderField("Content-Type"));
-        
-
+        //Invocacion al WS.
+        HttpsURLConnection.setDefaultSSLSocketFactory(sf);
+        SampleServiceService ejemplo = new SampleServiceService(new URL("https://localhost:7002/mvnjaxws-0.0.1-SNAPSHOT/SampleServiceService?WSDL"));
+        String respuesta = ejemplo.getSampleServicePort().sayHello("Sol");
+        System.out.println("Respuesta:"+respuesta);
     }
 }
---
+++ src/com/sps/ws/client/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://mvnjaxws.scotth.com/")
+package com.sps.ws.client;
